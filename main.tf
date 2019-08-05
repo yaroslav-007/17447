ebd6eb63508998d68d4d43f60ca5c898c663be45
Yaroslav <yorimark@abv.bg>
2019-08-05 15:28:45 +0300

    Getting ready for the last pull request

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
+resource "null_resource" "null" {}
+
+
 
 provider "shell" {}
 
 data "shell_script" "test" {
   lifecycle_commands {
     read = <<EOF
-		echo '{"commit_id": "'$(echo $(cat /etc/issue | awk '{print $1}'))'"}' >&3
+	#	echo '{"commit_id": "'$(echo $(cat /etc/issue | awk '{print $1}'))'"}' >&3
+  echo '{"commit_id": "echo '$(date)'"}' >&3
 		EOF
   }
 }
